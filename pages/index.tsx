f30970cf7d8a327f4d0a7e75e7ef2448ec2a640e
Justin Tam <jt.tam514@gmail.com>
2023-10-18 13:26:12 -0400

    Test number-input with mantine Form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
-import { Button, Group } from "@mantine/core";
+
+import { TextInput, Checkbox, Button, Group, Box, NumberInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 export default function IndexPage() {
+  const form = useForm({
+    initialValues: {
+      number: '',
+    }
+  });
+
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <Box maw={340} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <NumberInput {...form.getInputProps('number')}/>
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
   );
 }
